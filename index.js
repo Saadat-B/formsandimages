a81a1e2cc3cf657789a49e3fc0d08581cf36137e
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-19 11:24:06 +0530

    Handling images locally

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const fileUpload = require("express-fileupload");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -7,7 +7,22 @@ app.set("view engine", "ejs");
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
+app.get("/myget", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+app.post("/mypost", (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+  res.send(req.body);
+});
 app.get("/mygetform", (req, res) => {
   res.render("getform");
 });
