36ad22f2fe88a90020dbbdaf270fc4eae006ba74
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-19 09:57:05 +0530

    Added ejs templates and bootstrap

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/mygetform", (req, res) => {
+  res.render("getform");
+});
+app.get("/mypostform", (req, res) => {
+  res.render("postform");
+});
+
 app.listen(4000, () => console.log("Server is running at port 4000..."));
